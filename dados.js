d08c1491f942c015d23deeedf14912fd8b708fc9
pibba <avenidabrasil284@gmail.com>
2025-12-02 23:44:25 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,13 @@
+{
+  customer: {
+    name: "Nome Completo",
+    phone: "(11) 99999-9999",
+    email: "email@exemplo.com",
+    cep: "00000-000",
+    address: "Rua Exemplo",
+    number: "123",
+    complement: "Apto 45",
+    city: "São Paulo",
+    state: "SP"
+  }
+}
